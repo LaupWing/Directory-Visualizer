9e8ee1456f5ce51accc0ef39884016cf11b2396a
LaupWing <loc_strict@hotmail.com>
2020-07-20 15:34:07 +0200

    get a basic removal system

--- directoryVisualizer.py
+++ directoryVisualizer.py
@@ -1,8 +1,14 @@
 import os
 
+unwanted = ['.git', 'node_modules']
 
 for root, dirs, files in os.walk(os.getcwd()):
     base = os.path.basename(root)
-    if base == 'inner_test2':
-        print('skip')
+    dirs[:] = list(filter(lambda x: x not in unwanted, dirs))
+    # if base == 'inner_test2':
+    #     print('skip')
+    print()
+    print(base)
+    print(dirs)
     print(files)
+    print()
