8e39e630ec0b7ea451c1f11fc147f0ed04b99851
LaupWing <loc_strict@hotmail.com>
2020-07-20 16:03:01 +0200

    trying to figer out how this works

--- directoryVisualizer.py
+++ directoryVisualizer.py
@@ -2,13 +2,29 @@ import os
 
 unwanted = ['.git', 'node_modules']
 
-for root, dirs, files in os.walk(os.getcwd()):
-    base = os.path.basename(root)
-    dirs[:] = list(filter(lambda x: x not in unwanted, dirs))
-    # if base == 'inner_test2':
-    #     print('skip')
-    print()
-    print(base)
-    print(dirs)
-    print(files)
-    print()
+path = os.getcwd()
+tree = {
+    "root": os.path.basename(path),
+    "directories": [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))],
+    "files": [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+}
+
+print(tree)
+# for root, dirs, files in os.walk(os.getcwd()):
+#     base = os.path.basename(root)
+#     dirs[:] = list(filter(lambda x: x not in unwanted, dirs))
+#     tree = {
+#         "root": base,
+#         "directories": dirs,
+#         "files": files
+#     }
+#     # print(tree)
+#     if root in tree["directories"]:
+#         print('go in')
+#     # if base == 'inner_test2':
+#     #     print('skip')
+#     print()
+#     print(base)
+#     print(dirs)
+#     print(files)
+#     print()
